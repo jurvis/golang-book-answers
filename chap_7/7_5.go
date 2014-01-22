eadcdab177ab51131e545af92eeb925d8295da5b
Jurvis Tan <jurvis.tan@me.com>
2014-01-22 11:24:06 +0800

    updated 7_5.go

--- chap_7/7_5.go
+++ chap_7/7_5.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
-func fib() func() int {
-    x, y := 0, 1
-    return func() (r int) {
-        r = x
-        x, y = y, x + y
-        return
-    }
+func fib(n uint) uint {
+	// check if n == 0 or n == 1
+  if n == 0 || n == 1 {
+  	return n
+  } else {
+  	return fib(n-1) + fib(n-2)
+  }
 }
 
 func main() {
-	f := fib()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+	// insert any value of n within the parantheses of fib() below
+	 fmt.Println(fib(10))
+	// if you want a generator, comment out the above section and drop the comment below
+	//var n uint
+	//for n = 1; n < 10; n++ {
+	//	fmt.Println(fib(n))
+	//}
 }
+
